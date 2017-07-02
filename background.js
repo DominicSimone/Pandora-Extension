0d8ddaf60314e2eb1cf7202564e50f9e73ae0199
Dominic Simone <dominicsimone@gmail.com>
2017-07-02 13:30:24 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,21 +1,14 @@
-function onCommand(command) {
+function onCommand(command) {	
     chrome.tabs.query({url: 'https://www.pandora.com/*'}, function(tabs) {
 	// Open a pandora tab if one does not exist yet. 
 	if (tabs.length === 0) {
 	    chrome.tabs.create({url: 'https://www.pandora.com'});
 	}
-	if(command === "play/pause"){
-	var code_click='\
-		if(document.getElementsByClassName("playButton")[0].getAttribute("style")=="display: block;"){\
-			document.getElementsByClassName("playButton")[0].click();\
-		}\
-		else{\
-			document.getElementsByClassName("pauseButton")[0].click();\
-		}\
-	';
-	}
+	if(command === "toggle_music"){
+		var code_click = 'document.getElementsByClassName("TunerControl PlayButton Tuner__Control__Button Tuner__Control__Play__Button")[0].click();';
+		}
 	else{
-		var code_click = 'document.getElementsByClassName("skipButton")[0].click()';
+		var code_click = 'document.getElementsByClassName("TunerControl SkipButton Tuner__Control__Button Tuner__Control__Skip__Button")[0].click();';
 	}
 	
 	// Apply command on all pandora tabs.
